2cf995b7f6f4a307e801e129813150ca9c225936
OKPE <136821035+ugbasawo@users.noreply.github.com>
2024-08-26 17:27:37 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
+-- init.sql
+
 CREATE DATABASE IF NOT EXISTS food_ordering;
 USE food_ordering;
 
+-- Create the users table
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -9,11 +12,7 @@ CREATE TABLE IF NOT EXISTS users (
   role ENUM('customer', 'vendor', 'admin') DEFAULT 'customer'
 );
 
--- Create a new user
-CREATE USER 'David'@'%' IDENTIFIED BY PASSWORD('*6C757D055806E6028786EE6557D055806E60287');
-
--- Grant all privileges to the new user on the food_ordering database
+-- Create a new MySQL user and grant privileges
+CREATE USER IF NOT EXISTS 'David'@'%' IDENTIFIED BY 'Okpe@123';
 GRANT ALL PRIVILEGES ON food_ordering.* TO 'David'@'%';
-
--- Apply changes
 FLUSH PRIVILEGES;
